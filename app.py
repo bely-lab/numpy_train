8e4b85fc79a2ac3ccbd86f7a703471ba76b172c6
BelayneshM <tslatebely@gmail.com>
2024-12-01 04:49:07 -0800

    To gran new brach I have to commit this

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from package_name.logger import logger
 from package_name.tools.calculator import Calculator
 
 if __name__ == "__main__":
-    logger.info(f"1+3={Calculator.add(1,3)}")
+    logger.info(f"18+3={Calculator.add(18,3)}")
+logger.info(f"18/3={Calculator.divide(18,3)}")
